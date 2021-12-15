78cf4f2887d14282793523678d7c0ee80e744364
tonyishangu <tonyishangu@gmail.com>
2021-12-15 14:44:55 +0300

    print, input and variables. made a band name generator

--- day1/printing.py
+++ day1/printing.py
@@ -1,16 +1,16 @@
 # printing strings in console in python
-# print("hello world!")
+print("hello world!")
 
 #writting in a new line
-# print("Morning my niggas\nyou all feeling good")
+print("Morning my niggas\nyou all feeling good")
 
 #concat
-# print("Hello" + " " + "Tonnie")
+print("Hello" + " " + "Tonnie")
 
 #input function
-# input("enter your name \n")
+input("enter your name \n")
 #to print out the input data
-# print("hello " + input("WHat is your name? "))
+print("hello " + input("WHat is your name? "))
 
 #variables
 input("WHat is your name? ")  
