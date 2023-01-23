14dfd621e034c5534a85a5198964a6d0bb2a376c
tonyishangu <tonyishangu@gmail.com>
2023-01-23 15:39:50 +0300

    shuffled password generator

--- Day5/password_gen/mian.py
+++ Day5/password_gen/mian.py
@@ -8,13 +8,37 @@ no_of_letters = int(input('Enter number of letters you want in your password\n')
 num = int(input('enter the number of numbers you want in your password\n'))
 sbl = int(input('Enter the number of symbols you want in your password\n'))
 
-ltr = random.sample(letters, no_of_letters)
-numb = random.sample(numbers, num)
-symbol = random.sample(symbols, sbl)
+# ltr = random.sample(letters, no_of_letters)
+# numb = random.sample(numbers, num)
+# symbol = random.sample(symbols, sbl)
+# password_list = ltr + numb + symbol
+# psw = ''
+# for i in password_list:
+#     psw = psw + i
+# print(psw)
+
+# password = ''
+# for char in range(1, no_of_letters + 1):
+#     password += random.choice(letters)
+# for char in range(1, num + 1):
+#     password += random.choice(numbers)
+# for char in range(1, sbl + 1):
+#     password += random.choice(symbols)
+# print(password)
 
 
-password_list = ltr + numb + symbol
-psw = ''
-for i in password_list:
-    psw = psw + i
-print(psw)
+# shuffled password
+password = []
+for char in range(1, no_of_letters + 1):
+    password.append(random.choice(letters))
+for char in range(1, num + 1):
+    password.append(random.choice(numbers))
+for char in range(1, sbl + 1):
+    password.append(random.choice(symbols))
+print(password)
+random.shuffle(password)
+print(password)
+my_password = ''
+for char in password:
+    my_password += char
+print(my_password)
