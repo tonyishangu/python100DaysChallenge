016608856d0e101594bad577fcca6f71aff3ef08
tonyishangu <tonyishangu@gmail.com>
2023-01-24 13:44:00 +0300

    working with while loops and functions

--- Day6/whileloop.py
+++ Day6/whileloop.py
@@ -1,3 +1,20 @@
 # syntax
 # while something is true:
-#     do something 
+#     do something repeatedly
+x = 10
+while x > 0:
+    x -= 1
+    print(x)
+
+
+y = 100
+while y > 0:
+    if y % 5 == 0 and y % 3 == 0:
+        print('fizzbuzz')
+    elif y % 3 == 0:
+        print('fizz')
+    elif y % 5 == 0:
+        print('buzz')
+    else:
+        print(y)
+    y -= 1
