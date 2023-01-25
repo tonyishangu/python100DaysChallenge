db77327446b83fb080b50ca9bbc67912290c0c72
tonyishangu <tonyishangu@gmail.com>
2023-01-25 16:37:25 +0300

    replace _ with correct characters

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -2,10 +2,21 @@ import random
 
 words = ['advert', 'apple', 'zebra', 'academy', 'fox']
 choosen_word = random.choice(words)
+
 print(choosen_word)
-guess = input('Guess a letter\n')
-for i in choosen_word:
-    if i == guess:
-        print(i)
-    else:
-        print('nope')
+length = len(choosen_word)
+print(length)
+display = []
+
+for _ in range(length):
+    display += '_'
+print(display)
+
+guess = input('Guess a letter\n').lower()
+
+for pos in range(length):
+    letter = choosen_word[pos]
+    if letter == guess:
+        display[pos] = letter
+
+print(display)
