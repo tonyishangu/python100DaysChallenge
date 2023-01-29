eaabc653f2e49af263585c43ef4321a218d3c7b5
tonyishangu <tonyishangu@gmail.com>
2023-01-29 22:44:45 +0300

    hangman

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -1,22 +1,29 @@
 import random
 
 words = ['advert', 'apple', 'zebra', 'academy', 'fox']
-choosen_word = random.choice(words)
 
-print(choosen_word)
-length = len(choosen_word)
-print(length)
-display = []
+end_game = False
+while not end_game:
+    choosen_word = random.choice(words)
 
-for _ in range(length):
-    display += '_'
-print(display)
+    print(choosen_word)
+    length = len(choosen_word)
+    print(length)
+    display = []
 
-guess = input('Guess a letter\n').lower()
+    for _ in range(length):
+        display += '_'
+    print(display)
 
-for pos in range(length):
-    letter = choosen_word[pos]
-    if letter == guess:
-        display[pos] = letter
+    guess = input('Guess a letter\n').lower()
 
-print(display)
+    for pos in range(length):
+        letter = choosen_word[pos]
+        if letter == guess:
+            display[pos] = letter
+
+    print(display)
+
+    if '_' not in display:
+        end_game = True
+        print('You won')
