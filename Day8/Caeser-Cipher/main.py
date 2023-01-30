7026116db73b16c14f816a408752afe08799a954
tonyishangu <tonyishangu@gmail.com>
2023-01-30 16:36:08 +0300

    enable user to continue and chech on none letters and amount of shift if its too large

--- Day8/Caeser-Cipher/main.py
+++ Day8/Caeser-Cipher/main.py
@@ -1,45 +1,33 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-              'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
 
-def ceaser_cipher(start_text, shift_num, c_direction):
-    end_text = ''
-    if c_direction == 'decode':
-        shift_num *= -1
-    for letter in start_text:
-        pos = alphabet.index(letter)
-        new_pos = pos + shift_num
-        end_text += alphabet[new_pos]
-    print(end_text)
+from art import logo
+print(logo)
 
-ceaser_cipher(start_text=text, shift_num=shift, c_direction=direction)
+should_end = False
+while not should_end:
 
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % 26
 
-# def encrypt(p_text, shift_no):
-#     word = ''
-#     for letter in p_text:
-#         pos = alphabet.index(letter)
-#         new_pos = pos + shift_no
-#         new_letter = alphabet[new_pos]
-#         word += new_letter
-#     print(f'encoded word is {word}')
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
-# def decryt(enc_word, shift_num):
-#     new_word = ''
-#     for letters in enc_word:
-#         position = alphabet.index(letters)
-#         new_position = position - shift_num
-#         new_word += alphabet[new_position]
-#     print(f'decoded word is {new_word}')
-
-# if direction == 'encode':
-#     encrypt(p_text=text, shift_no=shift)
-# elif direction == 'decode':
-#     decryt(enc_word=text, shift_num=shift)
-# else:
-#     print('Wrong input')
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
+    
