4bf44ef99a516cd8421d818647d7d27176293248
tonyishangu <tonyishangu@gmail.com>
2023-01-30 15:00:48 +0300

    cipher project encryption function

---
+++ Day8/Caeser-Cipher/main.py
@@ -0,0 +1,20 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+              'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(p_text, shift_no):
+    word = ''
+    for letter in p_text:
+        pos = alphabet.index(letter)
+        new_pos = pos + shift_no
+        new_letter = alphabet[new_pos]
+        word += new_letter
+    print(f'encoded word is {word}')
+
+encrypt(p_text=text, shift_no=shift)
