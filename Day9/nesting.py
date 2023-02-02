b3bdf2305701c7d52c63ca4d0eb9e9ce28f25150
tonyishangu <tonyishangu@gmail.com>
2023-02-02 11:58:43 +0300

    question on nesting

--- Day9/nesting.py
+++ Day9/nesting.py
@@ -25,3 +25,31 @@ trv = [
     'total_visits': 12
     },
 ]
+
+# qqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
+
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+
+new_entry = {}
+def add_new_country(name, num, city):
+    new_entry = {
+        'country': name,
+        'visits': num,
+        'cities': city
+    }
+    travel_log.append(new_entry)
+
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
