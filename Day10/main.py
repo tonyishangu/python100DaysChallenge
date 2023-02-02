9b898b9bd003877614e1be9a50787c241c1e7a73
tonyishangu <tonyishangu@gmail.com>
2023-02-02 15:57:50 +0300

    working with multiple return statements

--- Day10/main.py
+++ Day10/main.py
@@ -5,7 +5,20 @@
 f_name = input('enter your first name\n')
 l_name = input('enter your last name\n')
 
+# def format_name(f_name, l_name):
+#     first = f_name.title()
+#     last = l_name.title()
+#     name = f'{first} {last}'
+#     print(name)
+#     return name
+
+# format_name(f_name=f_name, l_name=l_name)
+
+# MULTIPLE RETURNS
+
 def format_name(f_name, l_name):
+    if f_name == '' or l_name == '':
+        return print('invalid input')
     first = f_name.title()
     last = l_name.title()
     name = f'{first} {last}'
