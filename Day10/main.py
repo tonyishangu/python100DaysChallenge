c2614ba12a926e2f6950126f78897df96df6c180
tonyishangu <tonyishangu@gmail.com>
2023-02-02 15:46:33 +0300

    working with functions with outputs

---
+++ Day10/main.py
@@ -0,0 +1,15 @@
+# FUNCTIONS WITH OUTPUT
+# def mu_function():
+#   result = a*b
+#   return result
+f_name = input('enter your first name\n')
+l_name = input('enter your last name\n')
+
+def format_name(f_name, l_name):
+    first = f_name.title()
+    last = l_name.title()
+    name = f'{first} {last}'
+    print(name)
+    return name
+
+format_name(f_name=f_name, l_name=l_name)
