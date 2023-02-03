4072a4dd1f8655a256face3d7b1a155fd41530fa
tonyishangu <tonyishangu@gmail.com>
2023-02-03 13:21:15 +0300

    calculate score and refactor the code

--- Day11/main.py
+++ Day11/main.py
@@ -8,13 +8,6 @@ def pick_card():
     card = random.choice(cards)
     return card
 
-dealer_card = []
-player_card = []
-
-for _ in range(2):
-    player_card.append(pick_card())
-    dealer_card.append(pick_card())
-
 def calc_score(cards):
     '''return sum of the cards dealed'''
     if sum(cards) == 21 and len(cards) == 2:
@@ -22,5 +15,24 @@ def calc_score(cards):
     if 11 in cards and sum(cards) > 21:
         cards.remove(11)
         cards.append(1)
-        
+         
     return sum(cards)
+
+
+dealer_card = []
+player_card = []
+game_over = False
+
+for _ in range(2):
+    player_card.append(pick_card())
+    dealer_card.append(pick_card())
+    print(f'player cards {player_card}')
+    print(f'dealer cards {dealer_card[0]}')
+
+player_score = calc_score(player_card)
+dealer_score = calc_score(dealer_card)
+print(f'player score: {player_score}')
+print(f'dealer score: {dealer_score}')
+
+if player_score == 0 or dealer_score == 0 or player_score > 21:
+    game_over = True
