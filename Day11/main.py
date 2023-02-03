98c7f0e19bdb5d10fb111e4135d37f8d0f955f4b
tonyishangu <tonyishangu@gmail.com>
2023-02-03 12:41:30 +0300

    check if dealer or player has a black jack aready

--- Day11/main.py
+++ Day11/main.py
@@ -14,3 +14,13 @@ player_card = []
 for _ in range(2):
     player_card.append(pick_card())
     dealer_card.append(pick_card())
+
+def calc_score(cards):
+    '''return sum of the cards dealed'''
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+        
+    return sum(cards)
