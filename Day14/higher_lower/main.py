dffbd939712c2bad17e1eea065be26d0fa020e48
tonyishangu <tonyishangu@gmail.com>
2023-02-08 16:07:06 +0300

    pseudocode for the project

---
+++ Day14/higher_lower/main.py
@@ -0,0 +1,11 @@
+# display art
+# generate random data
+# formart the data
+# input guess
+# check if user is correct
+# ##----get folloers
+#  ###use if statement to check if user is correct
+# give user feedback
+# score keeping
+# make game repeatable
+# clear screen between rounds
