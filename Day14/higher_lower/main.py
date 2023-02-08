b6afcba12c18aa6e7a71459706513c466132fc03
tonyishangu <tonyishangu@gmail.com>
2023-02-08 16:15:25 +0300

    generate the  2 accounts

--- Day14/higher_lower/main.py
+++ Day14/higher_lower/main.py
@@ -1,6 +1,19 @@
+from art import logo, vs
+from game_date import data
+import random
 # display art
-# generate random data
+print(logo)
+# generate 2 random accounts from the data
+acc_a = random.choice(data)
+acc_b = random.choice(data)
+if acc_a == acc_b:
+    acc_b = random.choice(data)
+
 # formart the data
+acc_name = acc_a['name']
+acc_desc = acc_a['description']
+acc_country = acc_a['country']
+acc_followers = acc_a['follower_count']
 # input guess
 # check if user is correct
 # ##----get folloers
