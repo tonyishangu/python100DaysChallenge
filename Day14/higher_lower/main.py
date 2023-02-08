f915dc813585a907ad9611bd470a4b3cc2e3bae0
tonyishangu <tonyishangu@gmail.com>
2023-02-08 16:23:01 +0300

    function to format the accounts generated

--- Day14/higher_lower/main.py
+++ Day14/higher_lower/main.py
@@ -1,6 +1,16 @@
 from art import logo, vs
 from game_date import data
 import random
+
+
+def format_data(acc):
+    '''formart the data'''
+    acc_name = acc['name']
+    acc_desc = acc['description']
+    acc_country = acc['country']
+    # acc_followers = acc_a['follower_count']
+    return f'{acc_name}, a {acc_desc}, from {acc_country}'
+
 # display art
 print(logo)
 # generate 2 random accounts from the data
@@ -9,11 +19,12 @@ acc_b = random.choice(data)
 if acc_a == acc_b:
     acc_b = random.choice(data)
 
-# formart the data
-acc_name = acc_a['name']
-acc_desc = acc_a['description']
-acc_country = acc_a['country']
-acc_followers = acc_a['follower_count']
+# formated data
+print(f'Compare A: {format_data(acc_a)}')
+print(vs)
+print(f'Compare B: {format_data(acc_b)}')
+
+
 # input guess
 # check if user is correct
 # ##----get folloers
