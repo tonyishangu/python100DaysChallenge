90f495647f47505334832703c50f65c7729a3737
tonyishangu <tonyishangu@gmail.com>
2023-02-09 10:41:59 +0300

    make the game continious

--- Day14/higher_lower/main.py
+++ Day14/higher_lower/main.py
@@ -17,36 +17,44 @@ def check_answer(choice, a_followers, b_followers):
         return choice == 'a'
     else:
         return choice == 'b'
-    
+
+# score keeping  
 score = 0
 # display art
 print(logo)
-# generate 2 random accounts from the data
-acc_a = random.choice(data)
-acc_b = random.choice(data)
-if acc_a == acc_b:
-    acc_b = random.choice(data)
 
-# formated data
-print(f'Compare A: {format_data(acc_a)}')
-print(vs)
-print(f'Against B: {format_data(acc_b)}')
-
-# input guess
-choice = input('Who has more follers, A or B ?').lower()
-# check if user is correct
-# ##----get followers
-a_follower_count = acc_a['follower_count']
-b_follower_count = acc_b['follower_count']
-# ---/check if user is correct
-is_correct = check_answer(choice, a_follower_count, b_follower_count)
-
-# give user feedback
-if is_correct:
-    score += 1
-    print(f'You are right, your score is {score}')
-else:
-    print(f'You are wrong haha!!!, Total score is {score}')
-# score keeping
 # make game repeatable
-# clear screen between rounds
+game_still_on =  True
+acc_b = random.choice(data)
+
+while game_still_on:
+    # generate 2 random accounts from the data
+    acc_a = acc_b
+    acc_b = random.choice(data)
+    while acc_a == acc_b:
+        acc_b = random.choice(data)
+
+    # formated data
+    print(f'Compare A: {format_data(acc_a)}')
+    print(vs)
+    print(f'Against B: {format_data(acc_b)}')
+
+    # input guess
+    choice = input('Who has more follers, A or B ?').lower()
+    # check if user is correct
+    # ##----get followers
+    a_follower_count = acc_a['follower_count']
+    b_follower_count = acc_b['follower_count']
+    # ---/check if user is correct
+    is_correct = check_answer(choice, a_follower_count, b_follower_count)
+
+    # give user feedback
+    if is_correct:
+        score += 1
+        print(f'You are right, your score is {score}')
+    else:
+        game_still_on = False
+        print(f'You are wrong haha!!!, Total score is {score}')
+    
+
+    # clear screen between rounds
