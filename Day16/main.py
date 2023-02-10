64335c0366e7e1accc849d80de23e97f39e04969
tonyishangu <tonyishangu@gmail.com>
2023-02-10 10:56:53 +0300

    working with oop(class, objects and attributes)

---
+++ Day16/main.py
@@ -0,0 +1,30 @@
+# OBJECT ORIENTED PROOGRAMMING
+#       it's a method of structuring a program by bundling related 
+#       properties and behacviours into individual objects
+#       structure of a program
+
+# OBJECTS
+# car(object) = CarBlueprint()=>(class)
+# Example using TURTLE CLASS
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+
+# ATTRIBUTES OF AN OBJECT
+#       Data an object keeps track of
+#       syntax
+#       car(object).speed(attribute)
+#       Example using TURTLE CLASS
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
+# METHODS OF AN OBJECT
+#       Function tied to an object
+#       syntax
+#       car------->object.stop()------> mthod
+#       Example using TURTLE CLASS
+
+my_screen.exitonclick()
