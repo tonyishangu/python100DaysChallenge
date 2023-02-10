b1afb468dda25c85787fc642d212389a7cc4137d
tonyishangu <tonyishangu@gmail.com>
2023-02-10 12:44:03 +0300

    coffee machine using oop

---
+++ Day16/coffee/main.py
@@ -0,0 +1,27 @@
+from menu import Menu, MenuItem
+from money_machine import MoneyMachine
+from coffee_maker import CoffeeMaker
+
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+
+is_on = True
+
+
+
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f'What would you like? {options}\n').lower()
+    if choice == 'off':
+        is_on = False
+    elif choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
