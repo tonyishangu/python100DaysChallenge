e5fd51bebbc017abcb7ea5fcc1dcb86b284641fb
tonyishangu <tonyishangu@gmail.com>
2023-02-13 09:56:27 +0300

    creating attributes using a constructor and the init function

--- Day17/main.py
+++ Day17/main.py
@@ -4,6 +4,12 @@
 #   name using pascal case
 
 class User:
-    pass
 
-user_1 = User()
+    def __init__ (self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+
+
+user_1 = User('001', 'tony')
+print(user_1.id)
