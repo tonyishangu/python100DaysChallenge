d7d9963c05a500f669407e8d49381ba35b4a3261
tonyishangu <tonyishangu@gmail.com>
2023-02-20 06:21:47 +0300

    working with turtle module

---
+++ Day18/main.py
@@ -0,0 +1,48 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape('turtle')    #changing the shape
+timmy_the_turtle.color('green')
+for i in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
