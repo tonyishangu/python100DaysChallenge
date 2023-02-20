5a74b598e699e979659275031efb9abd5771e4ba
tonyishangu <tonyishangu@gmail.com>
2023-02-20 06:40:48 +0300

    function to draw a dotted line

--- Day18/main.py
+++ Day18/main.py
@@ -3,12 +3,18 @@ from turtle import Turtle, Screen
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape('turtle')    #changing the shape
 timmy_the_turtle.color('green')
+
+# draw a square
 for i in range(4):
     timmy_the_turtle.forward(100)
     timmy_the_turtle.right(90)
 
 
-
+for i in range(15):
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.pendown()
 
 
 
