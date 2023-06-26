81958b8fa6caad820b0b0be8f8889908efe23ffa
tonyishangu <tonyishangu@gmail.com>
2023-06-26 23:55:00 +0300

    add paddle and ball

--- day22/main.py
+++ day22/main.py
@@ -1,15 +1,31 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+import time
 
 paddle = Turtle()
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(width=800, height=600)
 screen.title('Pong')
+screen.tracer(0)
 
-paddle.shape('square')
-paddle.color('white')
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+r_pad = Paddle((350, 0))
+l_pad = Paddle((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_pad.go_up, 'Up')
+screen.onkey(r_pad.go_down, 'Down')
+screen.onkey(l_pad.go_up, 'w')
+screen.onkey(l_pad.go_down,'s')
+
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(.1)
+    screen.update()
+    ball.move()
 
 screen.exitonclick()
