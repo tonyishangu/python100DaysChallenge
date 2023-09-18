66a673e877d94d2cb9aa82e636e7ceceb736fb18
tonyishangu <tonyishangu@gmail.com>
2023-09-18 23:55:59 +0300

    advanced authentification using headers

--- Day37/main.py
+++ Day37/main.py
@@ -1,11 +1,31 @@
 import requests
 
+# --------------post request---------------
+
+username = 'saigelo'
+token = 'guukadvuiwfniuwef'
 endpoint = 'https://pixe.la/v1/users'
 user_params = {
-    'token':'guukadvuiwfniuwef',
-    'username': 'saigelo',
+    'token':token,
+    'username': username,
     'agreeTermsOfService': 'yes',
     'notMinor': 'yes'
 }
 res = requests.post(url=endpoint, json=user_params)
 print(res.text)
+
+# --------------creating a graph-----------
+graph_endpoint = f'{endpoint}/{username}/graphs'
+
+graph_params = {
+    'id': 'graphtest',
+    'name': 'Cycling Graph',
+    'unit': 'km',
+    'type': 'float',
+    'color': 'ajisai',
+}
+headers = {
+    'X-USER-TOKEN': token
+}
+response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+print(response.text)
